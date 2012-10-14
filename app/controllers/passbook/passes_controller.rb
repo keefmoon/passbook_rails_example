027ef86ca930695505624eb53b7781c7a5448ea0
Mattt Thompson <m@mattt.me>
2012-10-14 12:09:06 -0700

    Use #head rather than render :nothing => true
    Fix error where passes#show check for nil should come before check fro authorized

--- app/controllers/passbook/passes_controller.rb
+++ app/controllers/passbook/passes_controller.rb
@@ -4,14 +4,13 @@ class Passbook::PassesController < ApplicationController
   # Get the latest version of a pass.
   def show
     @pass = Passbook::Pass.where(pass_type_identifier: params[:pass_type_identifier], serial_number: params[:serial_number]).first
-    render nothing: true, status: 401 and return if request.env['HTTP_AUTHORIZATION'] != "ApplePass #{@pass.authentication_token}"
+    head :not_found and return if @pass.nil?
+    head :unauthorized and return if request.env['HTTP_AUTHORIZATION'] != "ApplePass #{@pass.authentication_token}"
 
-    if @pass.nil?
-      respond_with status: 404
-    elsif stale?(last_modified: @pass.updated_at.utc)
+    if stale?(last_modified: @pass.updated_at.utc)
       respond_with @pass
     else
-      respond_with status: 304
+      head :not_modified
     end
   end
 end
--- app/controllers/passbook/registrations_controller.rb
+++ app/controllers/passbook/registrations_controller.rb
@@ -4,7 +4,7 @@ class Passbook::RegistrationsController < ApplicationController
   # Get the serial numbers for passes associated with a device.
   def index
     @pass = Passbook::Pass.where(pass_type_identifier: params[:pass_type_identifier]).first
-    render nothing: true, status: 404 and return if @pass.nil?
+    head :not_found and return if @pass.nil?
 
     @registrations = @pass.registrations.where(device_library_identifier: params[:device_library_identifier])
     @registrations = @registrations.where("updated_at >= :passes_updated_since", {passes_updated_since: params[:passes_updated_since]}) if params[:passes_updated_since]
@@ -12,37 +12,37 @@ class Passbook::RegistrationsController < ApplicationController
     if @registrations.any?
       respond_with({lastUpdated: @registrations.maximum(:updated_at), serialNumbers: @registrations.collect(&:pass).collect(&:serial_number)})
     else
-      render nothing: true, status: 204
+      head :no_content
     end
   end
 
   # Register a device to receive push notifications for a pass.
   def create
     @pass = Passbook::Pass.where(pass_type_identifier: params[:pass_type_identifier], serial_number: params[:serial_number]).first
-    render nothing: true, status: 404 and return if @pass.nil?
-    render nothing: true, status: 401 and return if request.env['HTTP_AUTHORIZATION'] != "ApplePass #{@pass.authentication_token}"
+    head :not_found and return if @pass.nil?
+    head :unauthorized and return if request.env['HTTP_AUTHORIZATION'] != "ApplePass #{@pass.authentication_token}"
 
     @registration = @pass.registrations.first_or_initialize(device_library_identifier: params[:device_library_identifier])
     @registration.push_token = params[:pushToken]
 
-    status = @registration.new_record? ? 201 : 200
+    status = @registration.new_record? ? :created : :ok
 
     @registration.save
 
-    render nothing: true, status: status
+    head status
   end
 
   # Unregister a device so it no longer receives push notifications for a pass.
   def destroy
     @pass = Passbook::Pass.where(pass_type_identifier: params[:pass_type_identifier], serial_number: params[:serial_number]).first
-    render nothing: true, status: 404 and return if @pass.nil?
-    render nothing: true, status: 401 and return if request.env['HTTP_AUTHORIZATION'] != "ApplePass #{@pass.authentication_token}"
+    head :not_found and return if @pass.nil?
+    head :unauthorized and return if request.env['HTTP_AUTHORIZATION'] != "ApplePass #{@pass.authentication_token}"
 
     @registration = @pass.registrations.where(device_library_identifier: params[:device_library_identifier]).first
-    render nothing: true, status: 404 and return if @registration.nil?
+    head :not_found and return if @registration.nil?
 
     @registration.destroy
 
-    render nothing: true, status: 200
+    head :ok
   end
 end
