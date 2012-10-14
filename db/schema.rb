e8a43e8d47f747be630e83476737c62fcc058031
Mattt Thompson <m@mattt.me>
2012-10-14 12:08:13 -0700

    Updating db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,6 @@ ActiveRecord::Schema.define(:version => 20121012202250) do
     t.string   "authentication_token"
   end
 
-  add_index "passes", ["pass_type_identifier", "serial_number"], :name => "index_passes_on_pass_type_identifier_and_serial_number"
-
   create_table "registrations", :force => true do |t|
     t.integer  "pass_id"
     t.string   "device_library_identifier"
@@ -37,7 +35,5 @@ ActiveRecord::Schema.define(:version => 20121012202250) do
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
   end
-
-  add_index "registrations", ["device_library_identifier"], :name => "index_registrations_on_device_library_identifier"
 
 end
