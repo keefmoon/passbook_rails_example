a71191d93599d1b18814f45436704d9b36bd4442
Albert Vernon <aev@vernon.nu>
2012-10-25 14:51:42 -0400

    Allow multiple devices to register for a pass.
    first_or_initialize was being called incorrectly, which caused the
    following bug: device A registers for a particular pass, then device B
    registers for the same pass, but device B's push token gets stored in
    device A's registration instead of creating a new registration.

--- app/controllers/passbook/registrations_controller.rb
+++ app/controllers/passbook/registrations_controller.rb
@@ -28,7 +28,7 @@ class Passbook::RegistrationsController < ApplicationController
     head :not_found and return if @pass.nil?
     head :unauthorized and return if request.env['HTTP_AUTHORIZATION'] != "ApplePass #{@pass.authentication_token}"
 
-    @registration = @pass.registrations.first_or_initialize(device_library_identifier: params[:device_library_identifier])
+    @registration = @pass.registrations.where(device_library_identifier: params[:device_library_identifier]).first_or_initialize
     @registration.push_token = params[:pushToken]
 
     status = @registration.new_record? ? :created : :ok
